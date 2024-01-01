4f489b2e99932f2b7804618bc7248c1075b3587f
Sameer Khadka <79829935+sameerkhadka@users.noreply.github.com>
2024-01-01 13:24:00 +0545

    2 4 5 7

---
+++ resources/views/includes/cta.blade.php
@@ -0,0 +1,26 @@
+<div class="share uk-position-fixed">
+    <a href="tel:{{setting('contact-us.phone')}}" target="blank">
+        <img src="{{asset('images/phone.png')}}" alt="">
+    </a>
+    <a href="viber://chat?number={{setting('contact-us.viber')}}" target="blank">
+        <img src="{{asset('images/viber.png')}}" alt="">
+    </a>
+    <a href="https://wa.me/{{setting('contact-us.whatsapp')}}" target="blank"> 
+        <img src="{{asset('images/whatsapp.png')}}" alt="">
+    </a>
+    <a href="{{setting('contact-us.messenger')}}" target="blank"> 
+        <img src="{{asset('images/messenger.png')}}" alt="">
+    </a>
+    <a href="" target="blank"> 
+        <img src="{{asset('images/imo.png')}}" alt="">
+    </a>
+</div>
+
+<div class="results__cta uk-hidden@s uk-position-fixed">
+    <a href="/images" target="blank">
+        <img src="{{asset('images/image-gallery (1).png')}}" alt="">
+    </a>
+    <a href="/videos" target="blank">
+        <img src="{{asset('images/play (4).png')}}" alt="">
+    </a>
+</div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -16,33 +16,6 @@
 @endsection
 
 @section('body')
-    <div class="share uk-position-fixed">
-        <a href="tel:{{setting('contact-us.phone')}}" target="blank">
-            <img src="{{asset('images/phone.png')}}" alt="">
-        </a>
-        <a href="viber://chat?number={{setting('contact-us.viber')}}" target="blank">
-            <img src="{{asset('images/viber.png')}}" alt="">
-        </a>
-        <a href="https://wa.me/{{setting('contact-us.whatsapp')}}" target="blank"> 
-            <img src="{{asset('images/whatsapp.png')}}" alt="">
-        </a>
-        <a href="{{setting('contact-us.messenger')}}" target="blank"> 
-            <img src="{{asset('images/messenger.png')}}" alt="">
-        </a>
-        <a href="" target="blank"> 
-            <img src="{{asset('images/imo.png')}}" alt="">
-        </a>
-    </div>
-    
-    <div class="results__cta uk-hidden@s uk-position-fixed">
-        <a href="/images" target="blank">
-            <img src="{{asset('images/image-gallery (1).png')}}" alt="">
-        </a>
-        <a href="/videos" target="blank">
-            <img src="{{asset('images/play (4).png')}}" alt="">
-        </a>
-    </div>
-
     <div class="home__video">
         <div class="video__wrap uk-position-relative">
             
@@ -204,7 +177,7 @@
                                {{strip_tags($blog->description)}}
                             </p>
                             <div>
-                                <a href="/blogs/{{$blog->slug}}" class="button">Read more</a>
+                            <a href="{{$blog->link ? $blog->link : '/blogs/' . $blog->slug }}" @if($blog->link) target="blank" @endif class="button">Read more</a>
                             </div>
                         </div>
                     </div>
